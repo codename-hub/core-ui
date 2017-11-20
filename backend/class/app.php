f848e3e59bd3d95d1a61ada3e65ab299f04a8095
Kevin Dargel <kevin@jocoon.de>
2017-11-20 10:44:54 +0100

    added app::requireAsset for adding js or css using requireJs or other tools

--- backend/class/app.php
+++ backend/class/app.php
@@ -34,4 +34,70 @@ class app extends \codename\core\app {
     throw new exception(self::EXCEPTION_CORE_UI_APP_ILLEGAL_CALL, exception::$ERRORLEVEL_FATAL);
   }
 
+  /**
+   * [protected description]
+   * @var array
+   */
+  protected static $requireJsAssets = array();
+
+  /**
+   * [protected description]
+   * @var [type]
+   */
+  protected static $requireJsAdded = false;
+
+  /**
+   * [requireAsset description]
+   * @param string $type  [description]
+   * @param string|string[] $asset [description]
+   */
+  public static function requireAsset(string $type, $asset) {
+    if($type === 'requirejs') {
+      // add requireJS if not already added
+      if(!self::$requireJsAdded) {
+        app::getResponse()->requireResource('js', 'assets/requirejs/require.js', 0);
+        app::getResponse()->requireResource('js', 'assets/require.config.js', 1);
+        /* app::getResponse()->requireResource('script',
+          "requirejs.config({
+              baseUrl: 'library',
+          });", 0
+        );*/
+        self::$requireJsAdded = true;
+      }
+
+      $assets = [];
+      if(is_array($asset)) {
+        $assets = $asset;
+      } else {
+        $assets = [$asset];
+      }
+
+      foreach($assets as $a) {
+        if(!in_array($a, self::$requireJsAssets)) {
+          app::getResponse()->requireResource('script', "require(['{$a}'])");
+          self::$requireJsAssets[] = $a;
+        }
+      }
+
+    } else if($type === 'requirecss') {
+      $type = 'css';
+      if(is_array($asset)) {
+        foreach($asset as $a) {
+          app::getResponse()->requireResource($type, $a);
+        }
+      } else {
+        app::getResponse()->requireResource($type, $asset);
+      }
+    } else {
+      // TODO: require each resource?
+      if(is_array($asset)) {
+        foreach($asset as $a) {
+          app::getResponse()->requireResource($type, $a);
+        }
+      } else {
+        app::getResponse()->requireResource($type, $asset);
+      }
+    }
+  }
+
 }
